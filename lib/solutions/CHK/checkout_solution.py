911c0cac4a41f30be4258388423de2b691209e61
sbx_user1051 <sbx_user1051@169.254.38.217>
2022-07-29 19:16:19 +0000

    Fri Jul 29 19:16:19 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,23 +8,28 @@ def checkout(skus):
 
     total_price = 0
 
-    
+    for shop_item in skus:
+        if shop_item not in ["A", "B", "C", "D"]:
+            return -1
+        else:
+            for item in skus:
+                if item == "A":
+                    a_count += 1
+                if item == "B":
+                    b_count += 1
+                if item == "C":
+                    c_count += 1
+                if item == "D":
+                    d_count += 1
 
-    for item in skus:
-        if item == "A":
-            a_count += 1
-        if item == "B":
-            b_count += 1
-        if item == "C":
-            c_count += 1
-        if item == "D":
-            d_count += 1
+            total_price = sum([a_count * 50, b_count * 30, c_count * 20, d_count * 15])
 
-    total_price = sum([a_count * 50, b_count * 30, c_count * 20, d_count * 15])
+            if a_count > 3:
+                total_price = - 20
+            if b_count > 3:
+                total_price = - 15
 
-    if a_count > 3:
-        total_price = - 20
-    if b_count > 3:
-        total_price = - 15
+    return total_price
+
 
 
