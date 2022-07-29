045f8ea8751cb1a008ef205db281d30372f25106
sbx_user1051 <sbx_user1051@169.254.38.217>
2022-07-29 19:36:38 +0000

    Fri Jul 29 19:36:38 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -14,33 +14,20 @@ def checkout(skus):
         else:
             for item in skus:
                 if item == "A":
+                    total_price += 50
                     a_count += 1
                 if item == "B":
+                    total_price += 30
                     b_count += 1
                 if item == "C":
+                    total_price += 20
                     c_count += 1
                 if item == "D":
+                    total_price += 15
                     d_count += 1
 
-            a_count = a_count * 50
-            print("A count == {}".format(a_count))
-            b_count = b_count * 30
-            print("B count == {}".format(b_count))
-            c_count = c_count * 20
-            print("C count == {}".format(c_count))
-            d_count = d_count * 15
-            print("D count == {}".format(d_count))
-
-            total_price = a_count + b_count + c_count + d_count
-
             if a_count == 3:
-                total_price = total_price - 20
+                total_price -=
 
-            if b_count == 3:
-                total_price = total_price - 15
 
     return total_price
-
-
-
-
