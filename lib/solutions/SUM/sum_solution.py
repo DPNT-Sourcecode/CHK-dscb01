3993371aacebd4a9d33c161b1d6f7bbe589ec164
sbx_user1051 <sbx_user1051@169.254.122.189>
2022-07-29 17:52:02 +0000

    Fri Jul 29 17:52:02 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,12 +1,13 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(int_one, int_two):
-    if int_one not in range(1, 100):
-        int_one = 0
-    if int_two not in range(1, 100):
-        int_two = 0
+    # if int_one not in range(1, 100):
+    #     int_one = 0
+    # if int_two not in range(1, 100):
+    #     int_two = 0
 
     total = sum(int_one, int_two)
 
     return total
+
 
 
